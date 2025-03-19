7f15cd795d518836c013e62acd3c776fa5247754
Fergal Riordan <fergalriordan333@gmail.com>
2025-03-19 16:50:31 +0000

    Tidied data loading method

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,23 +1,22 @@
 import pandas as pd
 from tqdm import tqdm
 
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score
+
 # Load data from Hugging Face dataset
-def load_data(threshold=1.0):
+def load_data():
     splits = {'train': 'train.csv', 'test': 'test.csv'}
     train_df = pd.read_csv("hf://datasets/osyvokon/pavlick-formality-scores/" + splits["train"])
     test_df = pd.read_csv("hf://datasets/osyvokon/pavlick-formality-scores/" + splits["test"])
-    binary_df = test_df[test_df['avg_score'].abs() > threshold]
 
-    # create a binary column for formality
-    binary_df['formal'] = binary_df['avg_score'].apply(lambda x: 1 if x > 0 else 0)
-
-    return train_df, test_df, binary_df
+    return train_df, test_df
 
 # Predict formality using a Hugging Facemodel
 def predict_formality(model, tokenizer, df, batch_size=4):
     id2formality = {0: "formal", 1: "informal"} # from model documentation on Hugging Face
 
     predicted_labels = [] # 0 for informal, 1 for formal (consistent with dataset labels)
+    predicted_logits = [] # store the confidence scores for each prediction
 
     for i in tqdm(range(0, len(df), batch_size)):
         texts = df['sentence'][i:i + batch_size].tolist()
@@ -36,10 +35,28 @@ def predict_formality(model, tokenizer, df, batch_size=4):
         output = model(**encoding)
 
         batch_predicted_labels = []
+        batch_predicted_logits = []
         for text_scores in output.logits.softmax(dim=1):
             score_dict = {id2formality[idx]: score for idx, score in enumerate(text_scores.tolist())}
-            batch_predicted_labels.append(1 if score_dict['formal'] > score_dict['informal'] else 0)
+            predicted_label = 1 if score_dict['formal'] > score_dict['informal'] else 0
+            batch_predicted_labels.append(predicted_label)
+            batch_predicted_logits.append(score_dict['formal'] if predicted_label == 1 else score_dict['informal'])
 
         predicted_labels.extend(batch_predicted_labels)
+        predicted_logits.extend(batch_predicted_logits)
 
-    return predicted_labels
+    return predicted_labels, predicted_logits
+
+# Calculate Confusion Matrix (Binary Classification)
+def calculate_confusion_matrix(y_true, y_pred):
+    return confusion_matrix(y_true, y_pred)
+
+# Calculate Metrics (Binary Classification)
+def calculate_metrics(y_true, y_pred):
+    # Calculate metrics for binary classification
+    accuracy = accuracy_score(y_true, y_pred)
+    precision = precision_score(y_true, y_pred)
+    recall = recall_score(y_true, y_pred)
+    f1 = f1_score(y_true, y_pred)
+    
+    return accuracy, precision, recall, f1
